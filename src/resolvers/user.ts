879ccb595e4d7f799b2e4078b6be8fcfac175aee
WRHR <wrhreeves@gmail.com>
2020-11-20 22:24:15 -0700

    Enable sessions with express using cookies

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from '../entities/User'
 import { MyContext } from 'src/types'
-import { Resolver, Mutation, InputType, Field, Arg, Ctx, ObjectType } from 'type-graphql'
+import { Resolver, Mutation, InputType, Field, Arg, Ctx, ObjectType, Query } from 'type-graphql'
 import argon2 from 'argon2'
 
 @InputType()
@@ -31,10 +31,22 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(()=> User, { nullable: true })
+  async me(
+    @Ctx() { req, em }: MyContext
+  ){
+    if(!req.session.userId){
+      return null
+    }
+
+    const user = await em.findOne(User, { id: req.session.userId })
+    return user
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if(options.username.length <= 2) {
       return {
@@ -76,13 +88,16 @@ export class UserResolver {
         }
       }
     }
+    
+    req.session.userId = user.id
+    
     return { user }
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse>{
     const user = await em.findOne(User, { username: options.username })
 
@@ -107,6 +122,8 @@ export class UserResolver {
         ]
       }
     }
+
+    req.session.userId = user.id
 
     return { user }
   }
