24280d60323afc0f04f6c4d5e3b678096398f44f
WRHR <wrhreeves@gmail.com>
2020-11-22 22:30:36 -0700

    Change server start

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ const main = async () => {
   apolloServer.applyMiddleware({ app })
 
   app.listen(4000, () => {
-    console.log('server started on localhost:4000')
+    console.log('server started on port 4000')
   })
 }
 
