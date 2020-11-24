e1bf929714130bcc9af18c5461c75cfc9a9876e2
WRHR <wrhreeves@gmail.com>
2020-11-23 21:51:42 -0700

    Enable cors

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
 import { Request, Response, Express } from 'express'
  
-export type MyContext {
+export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
   req: Request & {session: Express.Session}
   res: Response
